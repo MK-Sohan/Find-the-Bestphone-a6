4d3402e713d473bd1758939549df79a6fd224712
MK-Sohan <mksohan786@gmail.com>
2022-03-01 01:10:37 +0600

    third

--- java/phone.js
+++ java/phone.js
@@ -5,6 +5,7 @@ const searchphone=()=>{
     https://openapi.programming-hero.com/api/phones?search=${inputbox}
 
     `
+    console.log(url);
     fetch(url)
     .then(res=>res.json())
     .then(data=>searchresult(data.data))
@@ -13,9 +14,11 @@ const searchphone=()=>{
  const searchresult=phons=>{
     //  console.log(phons);
 const parantcontainer =document.getElementById('cards-container');
+document.getElementById('cards-container').textContent='';
 for(const phone of phons){
     console.log(phone);
     const div = document.createElement('div');
+    div.classList.add('allcards-container');
     div.innerHTML=`
     
     <div class="card">
@@ -24,7 +27,7 @@ for(const phone of phons){
     </div>
     
     <div class="phone-details">
-      <h4>Name:</h4>
+      <h4>Name:${phone.phone_name}</h4>
       <h4>Release Date:</h4>
       <h4>Sensore:</h4>
       <h4>Others:</h4>
